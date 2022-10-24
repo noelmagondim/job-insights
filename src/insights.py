3fdbbc54d3f86a48a7f36dcd659ceaf2e995228b
noelmagondim <noelmagondim@gmail.com>
2022-10-24 10:21:05 -0300

    Req 03: concluded

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,12 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs_list = read(path)
+    data_type = set()
+    for job in jobs_list:
+        if job["industry"]:
+            data_type.add(job["industry"])
+    return data_type
 
 
 def filter_by_industry(jobs, industry):
